f44dec0e7a040c4cb3d363231a90975b8ec61876
pulak-sarmah <pulaksarmah333@gmail.com>
2024-08-20 17:56:59 +0530

    chore: Configure sample-lib package and add to api and web apps

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { add } from '@parkease/sample-lib';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hello World! ${add(4, 7)}`;
   }
 }
