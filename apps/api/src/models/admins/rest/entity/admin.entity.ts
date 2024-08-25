cbc75f56ce30b7223a923455989ad444ee0335f1
pulak-sarmah <pulaksarmah333@gmail.com>
2024-08-25 11:06:42 +0530

    chore: Add admins module in REST and GraphQL

---
+++ apps/api/src/models/admins/rest/entity/admin.entity.ts
@@ -0,0 +1,9 @@
+import { Admin } from '@prisma/client';
+
+import { RestrictProperties } from 'src/common/dtos/common.input';
+
+export class AdminEntity implements RestrictProperties<AdminEntity, Admin> {
+  uid: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
